9eb39054420dd5aaa163ea7f0877877a892b67bd
幻幻Fate <243414454@qq.com>
2017-11-17 17:32:31 +0800

    更新熊掌号方法

--- src/main/java/cn/reinforce/plugin/util/SEOUtil.java
+++ src/main/java/cn/reinforce/plugin/util/SEOUtil.java
@@ -1,6 +1,7 @@
 package cn.reinforce.plugin.util;
 
 import cn.reinforce.plugin.util.entity.HttpResult;
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.entity.StringEntity;
 import org.apache.log4j.Logger;
 
@@ -22,12 +23,49 @@ public class SEOUtil {
     /**
      * 主动提交网址到百度
      *
-     * @param urls     要提交的url
-     * @param site     网站地址
-     * @param token    百度的token
+     * @param urls  要提交的url
+     * @param site  网站地址
+     * @param token 百度的token
      */
     public static void baiduUrls(String urls, String site, String action, String token) {
         String url = "http://data.zz.baidu.com/" + action + "?site=" + site + "&token=" + token;
+
+        StringEntity entity = null;
+        try {
+            entity = new StringEntity(urls);
+        } catch (UnsupportedEncodingException e) {
+            LOG.error("编码错误", e);
+        }
+
+        HttpResult result = HttpClientUtil.postByEntity(url, entity);
+
+        System.out.println(result);
+    }
+
+    /**
+     * 熊掌号API提交
+     *
+     * @param urls
+     * @param appId
+     * @param action
+     * @param token
+     * @param original
+     */
+    public static void xzh(String urls, String appId, String action, String token, boolean original) {
+
+        String type = null;
+        switch (action) {
+            case "urls":
+                type = "realtime";
+                break;
+            case "update":
+                type = "batch";
+                break;
+            default:
+                break;
+        }
+
+        String url = "http://data.zz.baidu.com/urls?appid=" + appId + "&token=" + token + "&type=" + type + (original ? ",original" : "");
 
         StringEntity entity = null;
         try {
