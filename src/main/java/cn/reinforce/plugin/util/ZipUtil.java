f22520474cf77441525458cc9bcd7e999d8ca908
幻幻Fate <243414454@qq.com>
2018-02-26 17:41:07 +0800

    更新zip方法

--- src/main/java/cn/reinforce/plugin/util/ZipUtil.java
+++ src/main/java/cn/reinforce/plugin/util/ZipUtil.java
@@ -1,16 +1,20 @@
 package cn.reinforce.plugin.util;
 
-import org.slf4j.LoggerFactory;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.zip.CRC32;
 import java.util.zip.CheckedOutputStream;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 public class ZipUtil {
@@ -32,6 +36,114 @@ public class ZipUtil {
         String basedir = "";
         compress(file, out, basedir);
         out.close();
+    }
+
+    /**
+     * 解压缩zip包
+     *
+     * @param zipFilePath        zip文件的全路径
+     * @param unzipFilePath      解压后的文件保存的路径
+     * @param includeZipFileName 解压后的文件保存的路径是否包含压缩文件的文件名。true-包含；false-不包含
+     */
+    @SuppressWarnings("unchecked")
+    public static void unzip(String zipFilePath, String unzipFilePath, boolean includeZipFileName) {
+        File zipFile = new File(zipFilePath);
+        //如果解压后的文件保存路径包含压缩文件的文件名，则追加该文件名到解压路径
+        if (includeZipFileName) {
+            String fileName = zipFile.getName();
+            if (StringUtils.isNotEmpty(fileName)) {
+                fileName = fileName.substring(0, fileName.lastIndexOf("."));
+            }
+            unzipFilePath = unzipFilePath + File.separator + fileName;
+        }
+        //创建解压缩文件保存的路径
+        File unzipFileDir = new File(unzipFilePath);
+        if (!unzipFileDir.exists() || !unzipFileDir.isDirectory()) {
+            unzipFileDir.mkdirs();
+        }
+
+        //开始解压
+        ZipEntry entry = null;
+        String entryFilePath = null, entryDirPath = null;
+        File entryFile = null, entryDir = null;
+        int index = 0, count = 0, bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        ZipFile zip = null;
+        try {
+            zip = new ZipFile(zipFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Enumeration<ZipEntry> entries = (Enumeration<ZipEntry>) zip.entries();
+        //循环对压缩包里的每一个文件进行解压
+        while (entries.hasMoreElements()) {
+            entry = entries.nextElement();
+            //构建压缩包中一个文件解压后保存的文件全路径
+            entryFilePath = unzipFilePath + File.separator + entry.getName();
+            //构建解压后保存的文件夹路径
+            index = entryFilePath.lastIndexOf("/");
+            if (index != -1) {
+                entryDirPath = entryFilePath.substring(0, index);
+            } else {
+                index = entryFilePath.lastIndexOf(File.separator);
+                if (index != -1) {
+                    entryDirPath = entryFilePath.substring(0, index);
+                } else {
+                    entryDirPath = "";
+                }
+
+            }
+            System.out.println("entryDirPath:" + entryDirPath);
+            entryDir = new File(entryDirPath);
+            //如果文件夹路径不存在，则创建文件夹
+            if (!entryDir.exists() || !entryDir.isDirectory()) {
+                entryDir.mkdirs();
+            }
+
+            //创建解压文件
+            System.out.println(entryFilePath);
+            entryFile = new File(entryFilePath);
+            if (entryFile.exists()) {
+//                //检测文件是否允许删除，如果不允许删除，将会抛出SecurityException
+//                SecurityManager securityManager = new SecurityManager();
+//                securityManager.checkDelete(entryFilePath);
+                //删除已存在的目标文件
+                entryFile.delete();
+            }
+
+            try {
+                entryFile.createNewFile();
+                //写入文件
+                bos = new BufferedOutputStream(new FileOutputStream(entryFile));
+                bis = new BufferedInputStream(zip.getInputStream(entry));
+                while ((count = bis.read(buffer, 0, bufferSize)) != -1) {
+                    bos.write(buffer, 0, count);
+                }
+                bos.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(bos!=null){
+                    try {
+                        bos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(bis!=null){
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+
+        }
     }
 
     private static void compress(File file, ZipOutputStream out, String basedir) {
